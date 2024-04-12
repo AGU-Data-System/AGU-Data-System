331db901ddbb9b262a2185f755af949fdef45d3b
Sideghost <guilherme.cordeiro.pereira@gmail.com>
2024-04-12 20:52:34 +0100

    Components outline

---
+++ code/ts/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Router } from './router';
+
+const root = createRoot(document.getElementById('root')!);
+
+root.render(<Router/>)
+
---
+++ code/ts/src/router.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+
+  ]
+)
+
+export function Router() {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
