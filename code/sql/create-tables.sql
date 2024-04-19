a7a06a7830a45b0c093e52c0ceff6b17d5025799
48335 - Rodrigo Correia <a48335@alunos.isel.pt>
2024-04-19 17:06:11 +0100

    Changes to the DB Model

--- code/sql/create-tables.sql
+++ code/sql/create-tables.sql
@@ -5,32 +5,6 @@ create table if not exists transport_company
     name varchar primary key
 );
 
--- use for a rainy day
--- create table if not exists driver
--- (
---     company_name varchar,
---     name         varchar,
---
---     unique (name),
---
---     foreign key (company_name) references transport_company (name),
---
---     primary key (company_name, name)
--- );
---
--- create table if not exists car
--- (
---     company_name  varchar,
---     driver_name   varchar,
---     tank_capacity numeric(6, 3),
---     licence_plate varchar,
---
---     foreign key (company_name) references transport_company (name),
---     foreign key (driver_name) references driver (name),
---
---     primary key (company_name, licence_plate)
--- );
-
 create table if not exists loads
 (
     reference        varchar,
@@ -62,27 +36,17 @@ create table if not exists location
 (
     latitude  numeric(6, 3),
     longitude numeric(6, 3),
-    name      varchar default null,
+    name      varchar,
+    agu_id    int,
+    dno_id    int,
 
     constraint check_latitude check (latitude >= -180 and latitude <= 180),
     constraint check_longitude check (longitude >= -180 and longitude <= 180),
 
-    primary key (latitude, longitude)
-);
+    foreign key (agu_id, dno_id) references agu (id, dno_id),
 
--- create table if not exists temperature
--- (
---     id        int generated always as identity,
---     date      varchar,
---     min       numeric(6, 3),
---     max       numeric(6, 3),
---     timestamp bigint,
---     latitude  numeric(6, 3),
---     longitude numeric(6, 3),
---
---     foreign key (latitude, longitude) references location (latitude, longitude)
---
--- );
+    primary key (latitude, longitude, name, agu_id, dno_id) -- TODO: CHECK
+);
 
 create table if not exists agu
 (
@@ -98,8 +62,6 @@ create table if not exists agu
     capacity       numeric(6, 3),
     image          bytea,
     dno_id         int,
-    latitude       numeric(6, 3),
-    longitude      numeric(6, 3),
 
     unique (cui),
 
@@ -107,8 +69,6 @@ create table if not exists agu
     constraint min_level_check check (min_level >= 0 and min_level <= 100),
     constraint max_level_check check (max_level >= 0 and max_level <= 100),
 
-
-    foreign key (latitude, longitude) references location (latitude, longitude),
     foreign key (dno_id) references dno (id),
 
     primary key (dno_id, id)
@@ -132,8 +92,8 @@ create table if not exists contacts
 create table if not exists levels
 (
     timestamp bigint,
-    data     jsonb,
     type     varchar,
+    data     jsonb,
     provider_id int,
     agu_id int,
     dno_id int,
@@ -142,17 +102,18 @@ create table if not exists levels
 
     foreign key (agu_id, dno_id) references agu (id, dno_id),
 
-    primary key (timestamp, agu_id, dno_id)
+    primary key (timestamp, agu_id, dno_id, provider_id)
 );
 
 create table if not exists readings(
     timestamp bigint,
     agu_id int,
     dno_id int,
+    provider_id int,
 
-    foreign key (timestamp, agu_id, dno_id) references levels (timestamp, agu_id, dno_id),
+    foreign key (timestamp, agu_id, dno_id, provider_id) references levels (timestamp, agu_id, dno_id, provider_id),
 
-    primary key (timestamp, agu_id, dno_id)
+    primary key (timestamp, agu_id, dno_id, provider_id)
 );
 
 create table if not exists predictions
@@ -160,41 +121,74 @@ create table if not exists predictions
     timestamp bigint,
     agu_id int,
     dno_id int,
+    provider_id int,
     prediction_date bigint,
     model jsonb,
-    days_ahead int, -- calculated from prediction_date and timestamp
 
-    foreign key (timestamp, agu_id, dno_id) references levels (timestamp, agu_id, dno_id),
+    foreign key (timestamp, agu_id, dno_id, provider_id) references levels (timestamp, agu_id, dno_id, provider_id),
 
-    primary key (timestamp, agu_id, dno_id, prediction_date)
+    primary key (timestamp, agu_id, dno_id, provider_id, prediction_date)
 );
 
--- view for temperature and gas
-create or replace view temperature_view as
-    select levels.agu_id,
-           levels.provider_id,
-           levels.timestamp as fetch_timestamp,
-           levels.data as temperature,
-           predictions.prediction_date as date,
-           predictions.days_ahead
-    from levels join predictions
-        on predictions.agu_id = levels.agu_id and
-           predictions.timestamp = levels.timestamp and
-              predictions.dno_id = levels.dno_id
-           where levels.type = 'temperature';
-
-create or replace view gas_view as
-    select levels.agu_id,
-           levels.provider_id,
-           levels.timestamp as fetch_timestamp,
-           levels.data as level,
-           predictions.prediction_date as date,
-           predictions.days_ahead,
-           predictions.model
-    from levels join predictions
-        on predictions.agu_id = levels.agu_id and
-           predictions.timestamp = levels.timestamp and
-              predictions.dno_id = levels.dno_id
-           where levels.type = 'gas';
+-- -- view for temperature and gas
+-- -- needs refactoring
+-- -- join table levels with predictions and readings
+-- -- maybe needs function to check whether is prediction or reading based on timestamp and current day
+-- create or replace view temperature_view as
+-- select levels.agu_id,
+--        levels.provider_id,
+--        levels.timestamp as fetch_timestamp,
+--        levels.data as temperature,
+--        predictions.prediction_date as date,
+--        (predictions.prediction_date - levels.timestamp) as days_ahead
+-- from levels join predictions
+--                  on predictions.agu_id = levels.agu_id and
+--                     predictions.timestamp = levels.timestamp and
+--                     predictions.dno_id = levels.dno_id
+--             join readings
+--                  on readings.agu_id = levels.agu_id and
+--                     readings.timestamp = levels.timestamp and
+--                     readings.dno_id = levels.dno_id
+-- where levels.type = 'temperature';
+--
+-- create or replace view gas_view as
+-- select levels.agu_id,
+--        levels.provider_id,
+--        levels.timestamp as fetch_timestamp,
+--        levels.data as level,
+--        predictions.prediction_date as date,
+--        (predictions.prediction_date - levels.timestamp) as days_ahead,
+--        predictions.model
+-- from levels join predictions
+--                  on predictions.agu_id = levels.agu_id and
+--                     predictions.timestamp = levels.timestamp and
+--                     predictions.dno_id = levels.dno_id
+-- where levels.type = 'gas';
 
 commit;
+
+-- use for a rainy day
+-- create table if not exists driver
+-- (
+--     company_name varchar,
+--     name         varchar,
+--
+--     unique (name),
+--
+--     foreign key (company_name) references transport_company (name),
+--
+--     primary key (company_name, name)
+-- );
+--
+-- create table if not exists car
+-- (
+--     company_name  varchar,
+--     driver_name   varchar,
+--     tank_capacity numeric(6, 3),
+--     licence_plate varchar,
+--
+--     foreign key (company_name) references transport_company (name),
+--     foreign key (driver_name) references driver (name),
+--
+--     primary key (company_name, licence_plate)
+-- );
