dc5dbb181c1a811c84cb546132e9cc7c9d990580
Rodrigohcorreia <rodrigo.h.correia@gmail.com>
2024-05-03 16:25:30 +0100

    Rollbacks added providers if createAGU Fails

--- code/jvm/src/main/kotlin/aguDataSystem/server/service/agu/AGUService.kt
+++ code/jvm/src/main/kotlin/aguDataSystem/server/service/agu/AGUService.kt
@@ -3,6 +3,7 @@ package aguDataSystem.server.service.agu
 import aguDataSystem.server.domain.GasLevels
 import aguDataSystem.server.domain.agu.AGUCreationDTO
 import aguDataSystem.server.domain.agu.AGUDomain
+import aguDataSystem.server.domain.agu.AddProviderResult
 import aguDataSystem.server.domain.provider.GasProviderInput
 import aguDataSystem.server.domain.provider.ProviderType
 import aguDataSystem.server.domain.provider.TemperatureProviderInput
@@ -24,138 +25,157 @@ import org.springframework.stereotype.Service
  */
 @Service
 class AGUService(
-	private val transactionManager: TransactionManager,
-	val aguDomain: AGUDomain
+    private val transactionManager: TransactionManager,
+    val aguDomain: AGUDomain
 ) {
 
-	/**
-	 * Get an AGU by its CUI
-	 *
-	 * @param cui the CUI of the AGU
-	 * @return the AGU
-	 */
-	fun getAGUById(cui: String): GetAGUResult {
-		return transactionManager.run {
-			val agu = it.aguRepository.getAGUByCUI(cui) ?: return@run failure(GetAGUError.AGUNotFound)
+    /**
+     * Get an AGU by its CUI
+     *
+     * @param cui the CUI of the AGU
+     * @return the AGU
+     */
+    fun getAGUById(cui: String): GetAGUResult {
+        return transactionManager.run {
+            val agu = it.aguRepository.getAGUByCUI(cui) ?: return@run failure(GetAGUError.AGUNotFound)
 
-			success(agu)
-		}
-	}
+            success(agu)
+        }
+    }
 
-	/**
-	 * Create a new AGU
-	 *
-	 * @param creationAGU the AGU to create
-	 * @return the created AGU
-	 */
-	fun createAGU(creationAGU: AGUCreationDTO): AGUCreationResult {
-		isAGUDTOValid(creationAGU)?.let {
-			return it
-		}
+    /**
+     * Create a new AGU
+     *
+     * @param creationAGU the AGU to create
+     * @return the created AGU
+     */
+    fun createAGU(creationAGU: AGUCreationDTO): AGUCreationResult {
+        isAGUDTOValid(creationAGU)?.let {
+            return it
+        }
 
-		val aguBasicInfo = creationAGU.toAGUBasicInfo()
+        val aguBasicInfo = creationAGU.toAGUBasicInfo()
 
-		val temperatureUrl =
-			aguDomain.generateTemperatureUrl(aguBasicInfo.location.latitude, aguBasicInfo.location.longitude)
+        val temperatureUrl =
+            aguDomain.generateTemperatureUrl(aguBasicInfo.location.latitude, aguBasicInfo.location.longitude)
 
-		val gasProviderInput = GasProviderInput(aguBasicInfo.name, aguBasicInfo.gasLevelUrl)
-		val temperatureProviderInput = TemperatureProviderInput(aguBasicInfo.name, temperatureUrl)
-		val gasRes = aguDomain.addProviderRequest(gasProviderInput)
-		val tempRes = aguDomain.addProviderRequest(temperatureProviderInput)
+        val gasProviderInput = GasProviderInput(aguBasicInfo.name, aguBasicInfo.gasLevelUrl)
+        val temperatureProviderInput = TemperatureProviderInput(aguBasicInfo.name, temperatureUrl)
+        val gasRes = aguDomain.addProviderRequest(gasProviderInput)
+        val tempRes = aguDomain.addProviderRequest(temperatureProviderInput)
 
-		if (gasRes.isFailure() || tempRes.isFailure()) {
-			if (gasRes.isSuccess()) {
-				aguDomain.deleteProviderRequest(gasRes.getSuccessOrThrow())
-			}
-			if (tempRes.isSuccess()) {
-				aguDomain.deleteProviderRequest(tempRes.getSuccessOrThrow())
-			}
-			return failure(AGUCreationError.ProviderError)
-		}
+        if (gasRes.isFailure() || tempRes.isFailure()) {
+            return deleteSuccessProviders(gasRes, tempRes)
+        }
+        return try {
+            transactionManager.run {
+                val dno =
+                    it.dnoRepository.getByName(aguBasicInfo.dnoName) ?: return@run failure(AGUCreationError.InvalidDNO)
+                it.aguRepository.addAGU(aguBasicInfo, dno.id)
+                aguBasicInfo.tanks.forEach { tank ->
+                    it.tankRepository.addTank(aguBasicInfo.cui, tank)
+                }
+                aguBasicInfo.contacts.forEach { contact ->
+                    it.contactRepository.addContact(aguBasicInfo.cui, contact)
+                }
+                it.providerRepository.addProvider(aguBasicInfo.cui, gasRes.getSuccessOrThrow(), ProviderType.GAS)
+                it.providerRepository.addProvider(
+                    aguBasicInfo.cui,
+                    tempRes.getSuccessOrThrow(),
+                    ProviderType.TEMPERATURE
+                )
 
-		// TODO: Não sei o que fazer no caso do delete não funcionar
+                success(creationAGU.cui)
+            }
+        } catch (e: Exception) {
+            deleteSuccessProviders(gasRes, tempRes) //TODO: Add a log here that will log in case the deletion is not successful
+            throw e
+        }
 
-		return transactionManager.run {
-			val dno =
-				it.dnoRepository.getByName(aguBasicInfo.dnoName) ?: return@run failure(AGUCreationError.InvalidDNO)
-			it.aguRepository.addAGU(aguBasicInfo, dno.id)
-			aguBasicInfo.tanks.forEach { tank ->
-				it.tankRepository.addTank(aguBasicInfo.cui, tank)
-			}
-			aguBasicInfo.contacts.forEach { contact ->
-				it.contactRepository.addContact(aguBasicInfo.cui, contact)
-			}
-			it.providerRepository.addProvider(aguBasicInfo.cui, gasRes.getSuccessOrThrow(), ProviderType.GAS)
-			it.providerRepository.addProvider(aguBasicInfo.cui, tempRes.getSuccessOrThrow(), ProviderType.TEMPERATURE)
+    }
 
-			success(creationAGU.cui)
-		}
-	}
 
-	/**
-	 * Check if the AGU DTO is valid
-	 *
-	 * @param aguDTO the AGU DTO to check
-	 * @return the result of the check
-	 */
-	private fun isAGUDTOValid(aguDTO: AGUCreationDTO): AGUCreationResult? {
-		if (!aguDomain.isCUIValid(aguDTO.cui)) {
-			return failure(AGUCreationError.InvalidCUI)
-		}
+    /**
+     * Check if the AGU DTO is valid
+     *
+     * @param aguDTO the AGU DTO to check
+     * @return the result of the check
+     */
+    private fun isAGUDTOValid(aguDTO: AGUCreationDTO): AGUCreationResult? {
+        if (!aguDomain.isCUIValid(aguDTO.cui)) {
+            return failure(AGUCreationError.InvalidCUI)
+        }
 
-		if (!(aguDomain.areCoordinatesValid(aguDTO.location.latitude, aguDTO.location.longitude))) {
-			return failure(AGUCreationError.InvalidCoordinates)
-		}
+        if (!(aguDomain.areCoordinatesValid(aguDTO.location.latitude, aguDTO.location.longitude))) {
+            return failure(AGUCreationError.InvalidCoordinates)
+        }
 
-		ensureLevels(aguDTO.levels)?.let {
-			return it
-		}
+        ensureLevels(aguDTO.levels)?.let {
+            return it
+        }
 
-		if (aguDTO.tanks.isEmpty()) {
-			return failure(AGUCreationError.InvalidTank)
-		}
+        if (aguDTO.tanks.isEmpty()) {
+            return failure(AGUCreationError.InvalidTank)
+        }
 
-		aguDTO.tanks.forEach { tank ->
-			ensureLevels(tank.levels)?.let {
-				return it
-			}
-		}
+        aguDTO.tanks.forEach { tank ->
+            ensureLevels(tank.levels)?.let {
+                return it
+            }
+        }
 
-		aguDTO.contacts.forEach {
-			if (!aguDomain.isPhoneValid(it.phone) || it.name.isEmpty()) {
-				return failure(AGUCreationError.InvalidContact)
-			}
-			if (!aguDomain.isContactTypeValid(it.type)) {
-				return failure(AGUCreationError.InvalidContactType)
-			}
-		}
+        aguDTO.contacts.forEach {
+            if (!aguDomain.isPhoneValid(it.phone) || it.name.isEmpty()) {
+                return failure(AGUCreationError.InvalidContact)
+            }
+            if (!aguDomain.isContactTypeValid(it.type)) {
+                return failure(AGUCreationError.InvalidContactType)
+            }
+        }
 
-		return null
-	}
+        return null
+    }
 
-	/**
-	 * Ensure the levels are valid
-	 *
-	 * @param levels the levels to check
-	 * @return the result of the check
-	 */
-	private fun ensureLevels(levels: GasLevels): AGUCreationResult? {
-		if (!aguDomain.isPercentageValid(levels.min)) {
-			return failure(AGUCreationError.InvalidMinLevel)
-		}
+    /**
+     * Ensure the levels are valid
+     *
+     * @param levels the levels to check
+     * @return the result of the check
+     */
+    private fun ensureLevels(levels: GasLevels): AGUCreationResult? {
+        if (!aguDomain.isPercentageValid(levels.min)) {
+            return failure(AGUCreationError.InvalidMinLevel)
+        }
 
-		if (!aguDomain.isPercentageValid(levels.max)) {
-			return failure(AGUCreationError.InvalidMaxLevel)
-		}
+        if (!aguDomain.isPercentageValid(levels.max)) {
+            return failure(AGUCreationError.InvalidMaxLevel)
+        }
 
-		if (!aguDomain.isPercentageValid(levels.critical)) {
-			return failure(AGUCreationError.InvalidCriticalLevel)
-		}
+        if (!aguDomain.isPercentageValid(levels.critical)) {
+            return failure(AGUCreationError.InvalidCriticalLevel)
+        }
 
-		if (!aguDomain.areLevelsValid(levels)) {
-			return failure(AGUCreationError.InvalidLevels)
-		}
+        if (!aguDomain.areLevelsValid(levels)) {
+            return failure(AGUCreationError.InvalidLevels)
+        }
 
-		return null
-	}
+        return null
+    }
+
+    /**
+     * Delete the providers that were successfully added
+     *
+     * @param gasRes the result of the gas provider
+     * @param tempRes the result of the temperature provider
+     */
+    private fun deleteSuccessProviders(gasRes: AddProviderResult, tempRes: AddProviderResult): AGUCreationResult {
+        if (gasRes.isSuccess()) {
+            aguDomain.deleteProviderRequest(gasRes.getSuccessOrThrow())
+        }
+        if (tempRes.isSuccess()) {
+            aguDomain.deleteProviderRequest(tempRes.getSuccessOrThrow())
+        }
+
+        return failure(AGUCreationError.ProviderError)
+    }
 }
