4cd2ee2aacd030f81b4cb25cda3e1bb84566abc3
Sideghost <guilherme.cordeiro.pereira@gmail.com>
2024-05-14 00:21:24 +0100

    ChronService not working automatically.

--- code/jvm/src/main/kotlin/aguDataSystem/server/domain/provider/Provider.kt
+++ code/jvm/src/main/kotlin/aguDataSystem/server/domain/provider/Provider.kt
@@ -13,24 +13,4 @@ sealed class Provider {
 	abstract val id: Int
 	open val measures: List<Measure> = emptyList()
 	abstract val lastFetch: LocalDateTime?
-
-	/**
-	 * Returns the last reading based on the timestamp
-	 *
-	 * @return The last reading
-	 * @throws IllegalArgumentException if there are no readings
-	 */
-	fun getLatestReading(): Measure {
-		return getLatestReadingsOrEmpty().firstOrNull() ?: throw IllegalArgumentException("No readings found")
-	}
-
-	/**
-	 * Returns the last reading based on the timestamp
-	 * or null if there are no readings
-	 *
-	 * @return The last reading or null
-	 */
-	private fun getLatestReadingsOrEmpty(): List<Measure> {
-		return this.measures.filter { it.timestamp == this.lastFetch }
-	}
 }
